1f113c95352f04b32aab0fe6d52608b843d36d8c
Joe Zylstra <j-zylstra@hotmail.com>
2024-02-11 14:52:55 +0000

    "adding catch all route"

--- server.cjs
+++ server.cjs
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 
 
@@ -220,7 +220,7 @@ app.get('/reviews/DB', async (req, res) => {
 
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname,'index.html'));
 });
 
 app.listen(PORT, ()=> {

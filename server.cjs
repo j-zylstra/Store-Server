bc12927960123d2ebc0df2823a4626fc331ec94a
Joe Zylstra <j-zylstra@hotmail.com>
2024-02-12 11:45:39 +0000

    "restore sessions"

--- server.cjs
+++ server.cjs
@@ -2,7 +2,7 @@ const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex')
-// const session = require('cookie-session');
+const session = require('cookie-session');
 const bodyParser = require('body-parser');
 const path = require('path');
 

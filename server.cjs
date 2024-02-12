63393546caf2b49f99f6f33f2fce2a9694522756
Joe Zylstra <j-zylstra@hotmail.com>
2024-02-12 11:35:53 +0000

    "changed to sale.html"

--- server.cjs
+++ server.cjs
@@ -48,7 +48,7 @@ app.get('/', (req, res) => {
   });
 
   app.get('/sale', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'sale.html'));
   });
   
 
